f550afb800be60de76c416110bd7b624c7da84e6
Luke Parker <moongateuk@gmail.com>
2022-02-26 13:48:19 +0000

    Updates BuildInputConfig. Adds InputListener projects.

---
+++ src/InputListener/InputListener.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="InputListener.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace LPSoft.Stride.InputExtensions.InputListener
+{
+    using global::Stride.Input;
+
+    /// <summary>
+    /// Waits for input events from Strides <see cref="IInputEventListener"/>.
+    /// </summary>
+    public class InputListener
+    {
+    }
+}
